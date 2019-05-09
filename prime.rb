269199253fe5898a739fee5de44f817c7cbf3810
Devin Arnold <dbarnold321@gmail.com>
2019-05-09 00:24:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,8 @@
 # Add  code here!
 def prime?(num)
 (2..(num -1)).each do |n|
-  return false if num % n == 0 or num == - n
+  return false if num % n == 0
+  return false if num == - 
 end
 true
 end
